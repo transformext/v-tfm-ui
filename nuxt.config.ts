2d2d792e1797121c17c7e5c5cb63620a16936ab3
anirutkamchai <anirut.workspace@gmail.com>
2023-12-03 14:11:29 +0700

    :tada: scss pregenerate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,31 @@
+import fs from "fs";
+
+function prepareVurtifyOverrideStyle() {
+  const override = `
+  @forward "vuetify/settings" with (
+    // begin button configuration
+    $button-border-radius: 8px,
+    // end button configuration
+    $utilities: false,
+  );  
+  `;
+  if (!fs.existsSync("./assets/style/tfm")) {
+    fs.mkdirSync("./assets/style/tfm");
+  }
+  if (fs.existsSync("./assets/style/tfm/settings.scss")) {
+    const old = fs.readFileSync("./assets/style/tfm/settings.scss");
+    if (old.toString("utf-8") === override) return;
+  }
+  fs.writeFileSync("./assets/style/tfm/settings.scss", override);
+  console.log("re write vuetify override style");
+}
+
+prepareVurtifyOverrideStyle();
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["vuetify-nuxt-module"],
-  css: ["vuetify/styles", "./assets/style/settings.scss"],
+  css: ["vuetify/styles", "./assets/style/tfm/settings.scss"],
   vuetify: {
     vuetifyOptions: {
       defaults: {
@@ -9,10 +33,7 @@ export default defineNuxtConfig({
       },
     },
     moduleOptions: {
-      treeshaking: true,
-      styles: { configFile: "./assets/style/settings.scss" },
-      // autoImport: false,
-      useVuetifyLabs: true,
+      styles: { configFile: "./assets/style/tfm/settings.scss" },
     },
   },
 });
