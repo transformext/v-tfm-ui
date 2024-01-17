e473f12424c63e64888d5c1d607be181f4acf976
Nattaweekan.w <31063282+tlnattaweekan@users.noreply.github.com>
2024-01-17 21:00:04 +0700

    feat: :construction: wip base button

--- app.vue
+++ app.vue
@@ -1,6 +1,33 @@
 <template>
-  <div>
-    <v-btn>test</v-btn>
+  <div class="w-full h-screen flex flex-col justify-center items-center gap-4">
+    <p>Button</p>
+    <div class="grid grid-cols-4 gap-4">
+      <v-btn size="x-small">Button</v-btn>
+      <v-btn size="x-small" variant="outlined">Button</v-btn>
+      <v-btn size="x-small" variant="text">Button</v-btn>
+      <v-btn size="x-small" disabled>Button</v-btn>
+
+      <v-btn size="small">Button</v-btn>
+      <v-btn size="small" variant="outlined">Button</v-btn>
+      <v-btn size="small" variant="text">Button</v-btn>
+      <v-btn size="small" disabled>Button</v-btn>
+
+      <v-btn>Button</v-btn>
+      <v-btn variant="outlined">Button</v-btn>
+      <v-btn variant="text">Button</v-btn>
+      <v-btn disabled>Button</v-btn>
+
+      <v-btn size="large">Button</v-btn>
+      <v-btn size="large" variant="outlined">Button</v-btn>
+      <v-btn size="large" variant="text">Button</v-btn>
+      <v-btn size="large" disabled>Button</v-btn>
+
+      <v-btn size="x-large">Button</v-btn>
+      <v-btn size="x-large" variant="outlined">Button</v-btn>
+      <v-btn size="x-large" variant="text">Button</v-btn>
+      <v-btn size="x-large" disabled>Button</v-btn>
+    </div>
+
     <tfm-text-field :label="'label'" placeholder="test"></tfm-text-field>
   </div>
 </template>
